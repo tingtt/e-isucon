9dbe1f0c2c9f65a091d0c0775f33c0f2a45a8262
tingtt <ting.taku@gmail.com>
2023-01-23 17:54:30 +0900

    fix: fix db: `Specified key was too long`

--- backend/.mysql/init.sql
+++ backend/.mysql/init.sql
@@ -18,9 +18,9 @@ USE `prc_hub`;
 -- Table structure for table `users`
 --
 CREATE TABLE IF NOT EXISTS `users` (
-  `id` varchar(255) NOT NULL,
+  `id` varchar(128) NOT NULL,
   `name` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL UNIQUE,
+  `email` varchar(128) NOT NULL UNIQUE,
   `password` varchar(255) NOT NULL,
   `post_event_availabled` tinyint(1) NOT NULL DEFAULT 0,
   `manage` tinyint(1) NOT NULL DEFAULT 0,
@@ -35,13 +35,13 @@ CREATE TABLE IF NOT EXISTS `users` (
 -- Table structure for table `events`
 --
 CREATE TABLE IF NOT EXISTS `events` (
-  `id` varchar(255) NOT NULL,
+  `id` varchar(128) NOT NULL,
   `name` varchar(255) NOT NULL,
   `description` varchar(255),
   `location` varchar(255),
   `published` tinyint(1) NOT NULL,
   `completed` tinyint(1) NOT NULL,
-  `user_id` varchar(255) NOT NULL,
+  `user_id` varchar(128) NOT NULL,
   PRIMARY KEY (`id`)
 );
 
@@ -49,7 +49,7 @@ CREATE TABLE IF NOT EXISTS `events` (
 -- Table structure for table `event_datetimes`
 --
 CREATE TABLE IF NOT EXISTS `event_datetimes` (
-  `event_id` varchar(255) NOT NULL,
+  `event_id` varchar(128) NOT NULL,
   `start` datetime NOT NULL,
   `end` datetime NOT NULL,
   FOREIGN KEY (`event_id`) REFERENCES events(`id`) ON DELETE CASCADE
@@ -59,8 +59,8 @@ CREATE TABLE IF NOT EXISTS `event_datetimes` (
 -- Table structure for table `documents`
 --
 CREATE TABLE IF NOT EXISTS `documents` (
-  `id` varchar(255) NOT NULL,
-  `event_id` varchar(255) NOT NULL,
+  `id` varchar(128) NOT NULL,
+  `event_id` varchar(128) NOT NULL,
   `name` varchar(255) NOT NULL,
   `url` varchar(255) NOT NULL,
   PRIMARY KEY (`id`),
